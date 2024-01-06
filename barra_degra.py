937058b53575d4d0a975c6ebf27188c2941386ae
MiguelAngelCruzVargas <144273695+MiguelAngelCruzVargas@users.noreply.github.com>
2024-01-06 13:40:26 -0600

    Update barra_degra.py
    en esta version se corrio un poco la variacion del tiempo de carga

--- barra_degra.py
+++ barra_degra.py
@@ -13,7 +13,7 @@ frame.grid(columnspan=2, row=0)
 canvas = Canvas(frame, bg='black', width=545, height=485, bd=1)
 canvas.grid(padx=5, pady=5)
 
-inicio_tiempo = time.time()
+inicio_tiempo = time.perf_counter()
 duracion = 5
 
 def calculate_coordinates(nivel):
@@ -30,10 +30,12 @@ def draw_progress_bar(inicio_x, inicio_y, final_x, final_y, gradiente):
     canvas.create_line(inicio_x, inicio_y, final_x, final_y, fill=color, width=60)
 
 def draw_circles_and_text(centro_x, centro_y):
-    centro_x -= 30
-    centro_y -= 30
+    centro_x -= 30  # Ajusta según sea necesario
+    centro_y -= 30  # Ajusta según sea necesario
     canvas.create_oval(150 - 30, 150 - 30, 450 - 30, 450 - 30, fill='', outline='dark violet', width=5)
     canvas.create_oval(180 - 30, 180 - 30, 420 - 30, 420 - 30, fill='gray22', outline='dark violet', width=5)
+
+    centro_x += 35  # Puedes ajustar este valor según sea necesario
 
     texto = int((nivel / 3.6))
     texto = str(texto) + '%'
@@ -42,10 +44,12 @@ def draw_circles_and_text(centro_x, centro_y):
     canvas.create_text(centro_x, centro_y + 80, text='LA MANO', font=('Cambria Math', 25, 'bold'), fill='orange')
 
 def update_progress_bar():
-    global nivel
+    global nivel, inicio_tiempo
 
-    tiempo_actual = time.time()
+    tiempo_actual = time.perf_counter()
     tiempo_transcurrido = tiempo_actual - inicio_tiempo
+
+    print(f"Tiempo transcurrido: {tiempo_transcurrido:.4f} segundos")
 
     if tiempo_transcurrido < duracion:
         nivel = int((tiempo_transcurrido / duracion) * 360)
@@ -54,7 +58,6 @@ def update_progress_bar():
         canvas.create_oval(100, 100, 500, 500, fill="", outline='', width=5)
         inicio_x, inicio_y, final_x, final_y = calculate_coordinates(nivel)
 
-        # Calcular el valor del gradiente para el efecto degradado
         r = int((nivel / 360) * 255)
         g = 255 - r
         b = 0
@@ -64,18 +67,20 @@ def update_progress_bar():
         centro_x = (100 + 500) / 2 - 30
         centro_y = (100 + 500) / 2 - 30
         draw_circles_and_text(centro_x, centro_y)
-        ventana.after(10, update_progress_bar)
+
+        ventana.after(50, update_progress_bar)  # Aumenta el intervalo a 50 ms
     else:
         nivel = 360
         canvas.create_oval(100, 100, 500, 500, fill="", outline='', width=5)
         inicio_x, inicio_y, final_x, final_y = calculate_coordinates(nivel)
 
-        gradiente = (255, 0, 0)  # Color rojo
+        gradiente = (255, 0, 0)
         draw_progress_bar(inicio_x, inicio_y, final_x, final_y, gradiente)
 
         centro_x = (100 + 500) / 2 - 30
         centro_y = (100 + 500) / 2 - 30
         draw_circles_and_text(centro_x, centro_y)
+
         ventana.after(500, ventana.destroy)
 
 update_progress_bar()
